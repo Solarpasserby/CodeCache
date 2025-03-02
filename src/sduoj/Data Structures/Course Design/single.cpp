a5297f32f461870b580b871cc1c06807cb949c2a
Solarpasserby <yuruizhang188@gmail.com>
2025-03-02 13:14:07 +0800

    Data Structure homework 0.1

--- src/sduoj/Data Structures/Course Design/single.cpp
+++ src/sduoj/Data Structures/Course Design/single.cpp
@@ -13,17 +13,36 @@ struct InfInt
     InfInt(int n = 50) { value.reserve(n); }
     InfInt(const InfInt& other)
     {
-        for (uint8_t val: other.value)
+        value.clear();
+        for (const uint8_t& val: other.value)
             value.push_back(val);
     }
 
     void push_back(uint8_t val) { value.push_back(val); }
     int getLength() const { return value.size(); }
+    bool checkIfZero() const { return (value.size() == 1 && value.front() == 0); }
+    pair<uint8_t, uint8_t> digitMul(uint8_t a, uint8_t b)
+    {
+        pair<uint8_t, uint8_t> res;
+        res.first = a * b / 10;
+        res.second = a * b % 10;
+        return res;
+    }
+
+    void rmZero()
+    {
+        if(value.empty()) return;
+
+        for(auto it = value.rbegin(); (it + 1) != value.rend(); ++it)
+            if(*it == 0) value.pop_back();
+            else break;
+    }
+
 
     InfInt operator+(const InfInt& other) const
     {
         uint8_t addition = 0;
-        InfInt res(max(getLength(), other.getLength()));
+        InfInt res(max(50, max(getLength(), other.getLength()) + 1));
 
         vector<uint8_t>::const_iterator sit = value.begin(), oit = other.value.begin();
         while(sit != value.end() || oit != other.value.end())
@@ -42,8 +61,78 @@ struct InfInt
                 res.push_back(tmp - 10);
             }
         }
-
         if (addition) res.push_back(addition);
+
+        res.rmZero();
+        return res;
+    }
+
+    InfInt operator+=(const InfInt other)
+    {
+        uint8_t addition = 0;
+
+        if (other.checkIfZero()) return *this;
+
+        for(int i = getLength(); i < other.getLength(); i++)
+            value.push_back(0);
+
+        vector<uint8_t>::const_iterator it = other.value.begin();
+        for(uint8_t& val: value)
+        {
+            val += addition;
+            if (it != other.value.end()) val += *it++;
+            if (val < 10) addition = 0;
+            else
+            {
+                addition = 1;
+                val -= 10;
+            }
+        }
+        if (addition) value.push_back(addition);
+
+        this->rmZero();
+        return *this;
+    }
+
+    InfInt operator*(const InfInt other)
+    {
+        int times = 0, initLength = max(50, getLength() + other.getLength());
+        InfInt res(initLength);
+
+        if (other.checkIfZero() || this->checkIfZero())
+        {
+            res.push_back(0);
+            return res;
+        }
+
+        for(uint8_t mult: other.value)
+        {
+            InfInt tmp(initLength);
+            uint8_t addition = 0;
+            for(int i = 0; i < times; i++)
+                tmp.push_back(0);
+
+            for(uint8_t val: value)
+            {
+                auto digit = digitMul(mult, val);
+                digit.second += addition;
+                if (digit.second > 10)
+                {
+                    digit.second -= 10;
+                    digit.first += 1;
+                }
+                addition = digit.first;
+                tmp.push_back(digit.second);
+            }
+
+            if(addition) tmp.push_back(addition);
+            addition = 0;
+
+            res += tmp;
+            ++times;
+        }
+
+        res.rmZero();
         return res;
     }
 };
@@ -53,7 +142,8 @@ istream& operator>>(istream& is, InfInt& num)
     string tmp;
     is >> tmp;
     for(auto it = tmp.rbegin(); it != tmp.rend(); ++it)
-        num.push_back(*it - '0');
+        if(*it >= '0' && *it <= '9') num.push_back(*it - '0');
+        else throw invalid_argument("Invalid input: unable to read integer.");
     return is;
 }
 
@@ -71,7 +161,7 @@ int main()
 
     InfInt a, b;
     cin >> a >> b;
-    cout << a + b;
+    cout << a * b;
 
     return 0;
 }
